7dbf3d994c81c153b362dde490cb40b1bafb96a0
jgarcia <js8215001@gmail.com>
2024-02-23 16:56:35 -0600

    ajustes

--- app/src/main/java/com/udb/dsm104/desafiopractico1/clases/Salario.kt
+++ app/src/main/java/com/udb/dsm104/desafiopractico1/clases/Salario.kt
@@ -7,11 +7,11 @@ class Salario(
     val nombre: String,
     val salarioBase: BigDecimal
 ) {
-    val afp = BigDecimal(0.0725)
+    val afp = BigDecimal(0.04)
     val isss = BigDecimal(0.03)
     var salarioDespuesDescuentos: BigDecimal = salarioBase
     var salarioTotal = BigDecimal(0)
-    var renta = BigDecimal(0)
+    var renta = BigDecimal(0.05)
     public fun ObtenerSalarioTotal(): BigDecimal {
         return salarioTotal
     }
@@ -29,16 +29,16 @@ class Salario(
     }
 
     public fun CalcularRenta():BigDecimal{
-        println("Salario despues de descuentos -> " + salarioDespuesDescuentos)
-        if(salarioDespuesDescuentos >= BigDecimal(472.01) && salarioDespuesDescuentos <= BigDecimal(895.24)){
-            renta = BigDecimal(0.1)
-        }else if(salarioDespuesDescuentos >= BigDecimal(895.25) && salarioDespuesDescuentos <= BigDecimal(2038.1)){
-            renta = BigDecimal(0.2)
-        }else if(salarioDespuesDescuentos >= BigDecimal(2038.11)){
-            renta = BigDecimal(0.3)
-        }else{
-            renta = BigDecimal(0)
-        }
+//        println("Salario despues de descuentos -> " + salarioDespuesDescuentos)
+//        if(salarioDespuesDescuentos >= BigDecimal(472.01) && salarioDespuesDescuentos <= BigDecimal(895.24)){
+//            renta = BigDecimal(0.1)
+//        }else if(salarioDespuesDescuentos >= BigDecimal(895.25) && salarioDespuesDescuentos <= BigDecimal(2038.1)){
+//            renta = BigDecimal(0.2)
+//        }else if(salarioDespuesDescuentos >= BigDecimal(2038.11)){
+//            renta = BigDecimal(0.3)
+//        }else{
+//            renta = BigDecimal(0)
+//        }
         val retencionRenta = BigDecimal((salarioDespuesDescuentos * renta).toString()).setScale(2, RoundingMode.HALF_EVEN);
         salarioTotal = salarioDespuesDescuentos - retencionRenta
         return retencionRenta
