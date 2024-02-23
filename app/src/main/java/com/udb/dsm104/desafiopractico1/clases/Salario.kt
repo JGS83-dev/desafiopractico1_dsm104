13980d64a28828b25d4ed453bcbb34a1e853a986
jgarcia <js8215001@gmail.com>
2024-02-22 18:18:39 -0600

    Ejercicio 2 completado

--- app/src/main/java/com/udb/dsm104/desafiopractico1/clases/Salario.kt
+++ app/src/main/java/com/udb/dsm104/desafiopractico1/clases/Salario.kt
@@ -7,8 +7,40 @@ class Salario(
     val nombre: String,
     val salarioBase: BigDecimal
 ) {
+    val afp = BigDecimal(0.0725)
+    val isss = BigDecimal(0.03)
+    var salarioDespuesDescuentos: BigDecimal = salarioBase
+    var salarioTotal = BigDecimal(0)
+    var renta = BigDecimal(0)
+    public fun ObtenerSalarioTotal(): BigDecimal {
+        return salarioTotal
+    }
 
-    public fun CalcularSalario(): BigDecimal? {
-        return BigDecimal(0)
+    public fun CalcularAFP():BigDecimal{
+        val retencionAfp = BigDecimal((salarioBase * afp).toString()).setScale(2, RoundingMode.HALF_EVEN);
+        salarioDespuesDescuentos -= retencionAfp
+        return retencionAfp
+    }
+
+    public fun CalcularISSS():BigDecimal{
+        val retencionSeguro = BigDecimal((salarioBase * isss).toString()).setScale(2, RoundingMode.HALF_EVEN);
+        salarioDespuesDescuentos -= retencionSeguro
+        return retencionSeguro
+    }
+
+    public fun CalcularRenta():BigDecimal{
+        println("Salario despues de descuentos -> " + salarioDespuesDescuentos)
+        if(salarioDespuesDescuentos >= BigDecimal(472.01) && salarioDespuesDescuentos <= BigDecimal(895.24)){
+            renta = BigDecimal(0.1)
+        }else if(salarioDespuesDescuentos >= BigDecimal(895.25) && salarioDespuesDescuentos <= BigDecimal(2038.1)){
+            renta = BigDecimal(0.2)
+        }else if(salarioDespuesDescuentos >= BigDecimal(2038.11)){
+            renta = BigDecimal(0.3)
+        }else{
+            renta = BigDecimal(0)
+        }
+        val retencionRenta = BigDecimal((salarioDespuesDescuentos * renta).toString()).setScale(2, RoundingMode.HALF_EVEN);
+        salarioTotal = salarioDespuesDescuentos - retencionRenta
+        return retencionRenta
     }
 }
--- app/src/main/java/com/udb/dsm104/desafiopractico1/ui/ejercicio2/Ejercicio2Fragment.kt
+++ app/src/main/java/com/udb/dsm104/desafiopractico1/ui/ejercicio2/Ejercicio2Fragment.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.udb.dsm104.desafiopractico1.clases.PromedioNotas
+import com.udb.dsm104.desafiopractico1.clases.Salario
+import com.udb.dsm104.desafiopractico1.clases.Utils
 import com.udb.dsm104.desafiopractico1.databinding.FragmentEjercicio2Binding
 
 class Ejercicio2Fragment : Fragment() {
@@ -17,6 +22,11 @@ class Ejercicio2Fragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private lateinit var btnCalcular: Button
+    private lateinit var txtMensaje: TextView
+
+    private lateinit var txtSalario: EditText
+    private lateinit var txtNombre: EditText
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,11 +38,31 @@ class Ejercicio2Fragment : Fragment() {
         _binding = FragmentEjercicio2Binding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        ejercicio2ViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        btnCalcular = binding.BtnCalcular
+        txtMensaje = binding.TxtResultado
+
+        txtSalario = binding.TxtSalario
+        txtNombre = binding.TxtNombre
+
+        btnCalcular.setOnClickListener { realizarCalculoRenta() }
+
         return root
+    }
+
+    private fun realizarCalculoRenta() {
+        val utils = Utils()
+        val salario = Salario(
+            txtNombre.text.toString(),
+            utils.obtenerNumero(txtSalario),
+        )
+
+        var mensaje:String = "Estimado: " + salario.nombre
+        mensaje += "\nSu Salario base es de: $" + salario.salarioBase
+        mensaje += "\nSe le aplica un descuento de AFP de: $" + salario.CalcularAFP()
+        mensaje += "\nSe le aplica un descuento de ISSS de: $" + salario.CalcularISSS()
+        mensaje += "\nSe le aplica un descuento de Renta de: $" + salario.CalcularRenta()
+        mensaje += "\nSu Salario Total es de: $" + salario.ObtenerSalarioTotal()
+        txtMensaje.text = mensaje
     }
 
     override fun onDestroyView() {
